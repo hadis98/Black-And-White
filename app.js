57fe4c72f6803d4d46fc72496da4ec193e572eb1
hadis ghafouri <hadisghafouri98@gmail.com>
2021-05-14 23:52:03 +0430

    add navigation bar for mobile size and fix button problems

--- app.js
+++ app.js
@@ -41,6 +41,9 @@ const nextBtnNature = document.querySelector("#nature-next");
 const prevBtnPeople = document.querySelector("#people-prev");
 const nextBtnPeople = document.querySelector("#people-next");
 
+const navbar_icon = document.querySelector('.navbar-icon');
+const navbar_close_icon = document.querySelector('.close-menu');
+const menuLink_li = document.querySelectorAll('.menu-link-li');
 
 let curIdx_flowers = 0;
 let curIdx_nature = 0;
@@ -88,6 +91,26 @@ nextBtnPeople.addEventListener("click", () => {
     clickNextBtn(people);
 });
 
+navbar_icon.addEventListener('click', () => {
+    document.getElementById('menu2').style.display = "flex";
+    document.getElementById('menu2').classList.add('flex-menu')
+    navbar_close_icon.style.display = "block";
+    // navbar_icon.style.display = "none";
+})
+
+navbar_close_icon.addEventListener('click', () => {
+    document.getElementById('menu2').style.display = "none";
+    navbar_icon.style.display = "block";
+    navbar_close_icon.style.display = "none";
+
+})
+
+for (let i = 0; i < menuLink_li.length; i++) {
+    menuLink_li[i].addEventListener('click', () => {
+        navbar_close_icon.style.display = "none";
+        document.getElementById('menu2').style.display = "none";
+    })
+}
 
 function clickPrevBtn(array) {
     if (array === flowers) {
