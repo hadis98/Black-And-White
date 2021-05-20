b16361d7f520d6bb93a4f9a5cb81edd4aa0e7ec1
hadis ghafouri <hadisghafouri98@gmail.com>
2021-05-20 18:52:13 +0430

    update slideshow

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ const flowers_slideshow = document.getElementById("flowers-slide");
 const nature_slideshow = document.getElementById("nature-slide");
 const people_slideshow = document.getElementById("people-slide");
 
-const slideShow_container = document.querySelector('.slideShow__container');
+const slideShow_container = document.querySelector(".slideShow__container");
 const slideShow_img_flowers = document.querySelector("#flowers-slide img");
 const slideShow_img_nature = document.querySelector("#nature-slide img");
 const slideShow_img_people = document.querySelector("#people-slide img");
@@ -41,12 +41,22 @@ const nextBtnNature = document.querySelector("#nature-next");
 const prevBtnPeople = document.querySelector("#people-prev");
 const nextBtnPeople = document.querySelector("#people-next");
 
+const chooseImg_flowers_container =
+    document.getElementById("chooseImg_flowers");
+const chooseImg_nature_container =
+    document.getElementById("chooseImg_nature");
+const chooseImg_people_container =
+    document.getElementById("chooseImg_people");
+
 
 let curIdx_flowers = 0;
 let curIdx_nature = 0;
 let curIdx_people = 0;
 
 flowersBtn.addEventListener("click", () => {
+    chooseImg_nature_container.style.display = "none";
+    chooseImg_flowers_container.style.display = "grid";
+    chooseImg_people_container.style.display = "none";
     flowers_slideshow.style.display = "flex";
     people_slideshow.style.display = "none";
     nature_slideshow.style.display = "none";
@@ -55,12 +65,18 @@ flowersBtn.addEventListener("click", () => {
 
 natureBtn.addEventListener("click", () => {
     flowers_slideshow.style.display = "none";
+    chooseImg_nature_container.style.display = "grid";
+    chooseImg_flowers_container.style.display = "none";
+    chooseImg_people_container.style.display = "none";
     people_slideshow.style.display = "none";
     nature_slideshow.style.display = "flex";
     slideShow_img_nature.src = nature[curIdx_flowers];
 });
 
 peopleBtn.addEventListener("click", () => {
+    chooseImg_nature_container.style.display = "none";
+    chooseImg_flowers_container.style.display = "none";
+    chooseImg_people_container.style.display = "grid";
     flowers_slideshow.style.display = "none";
     nature_slideshow.style.display = "none";
     people_slideshow.style.display = "flex";
@@ -88,8 +104,72 @@ nextBtnPeople.addEventListener("click", () => {
     clickNextBtn(people);
 });
 
+for (let i = 0; i < flowers.length; i++) {
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add("column-img");
+    imgDiv.classList.add("column-img-flowers");
+    const image = document.createElement("img");
+    image.src = flowers[i];
+    imgDiv.appendChild(image);
+    chooseImg_flowers_container.appendChild(imgDiv);
+}
+for (let i = 0; i < nature.length; i++) {
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add("column-img");
+    imgDiv.classList.add("column-img-nature");
+    const image = document.createElement("img");
+    image.src = nature[i];
+    imgDiv.appendChild(image);
+    chooseImg_nature_container.appendChild(imgDiv);
+}
+for (let i = 0; i < people.length; i++) {
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add("column-img");
+    imgDiv.classList.add("column-img-people");
+    const image = document.createElement("img");
+    image.src = people[i];
+    imgDiv.appendChild(image);
+    chooseImg_people_container.appendChild(imgDiv);
+}
 
+const columnImages_flowers = document.querySelectorAll(".column-img-flowers");
+const columnImages_nature = document.querySelectorAll(".column-img-nature");
+const columnImages_people = document.querySelectorAll(".column-img-people");
+for (let i = 0; i < columnImages_flowers.length; i++) {
+    columnImages_flowers[i].addEventListener("click", () => {
+        changeColumnImage(i, columnImages_flowers, slideShow_img_flowers, flowers, 'flowers');
+    });
+}
 
+for (let i = 0; i < columnImages_nature.length; i++) {
+    columnImages_nature[i].addEventListener("click", () => {
+        changeColumnImage(i, columnImages_nature, slideShow_img_nature, nature, 'nature');
+    });
+}
+
+for (let i = 0; i < columnImages_people.length; i++) {
+    columnImages_people[i].addEventListener("click", () => {
+        changeColumnImage(i, columnImages_people, slideShow_img_people, people, 'people');
+    });
+}
+
+function changeColumnImage(index, array, slideShowimg, arr, type) {
+    for (i = 0; i < array.length; i++)
+        array[i].className = array[i].className.replace(
+            " active",
+            ""
+        );
+    array[index].classList += " active";
+    slideShowimg.src = arr[index];
+    if (type === 'flowers') {
+        curIdx_flowers = index;
+
+    } else if (type === 'nature') {
+        curIdx_nature = index;
+    } else if (type === 'people') {
+        curIdx_people = index;
+    }
+}
 
 function clickPrevBtn(array) {
     if (array === flowers) {
@@ -141,10 +221,28 @@ function clickNextBtn(array) {
 function changeImage(array) {
     if (array === flowers) {
         slideShow_img_flowers.src = flowers[curIdx_flowers];
+        for (i = 0; i < columnImages_flowers.length; i++)
+            columnImages_flowers[i].className = columnImages_flowers[i].className.replace(
+                " active",
+                ""
+            );
+        columnImages_flowers[curIdx_flowers].classList += " active";
     } else if (array === nature) {
         slideShow_img_nature.src = nature[curIdx_nature];
+        for (i = 0; i < columnImages_nature.length; i++)
+            columnImages_nature[i].className = columnImages_nature[i].className.replace(
+                " active",
+                ""
+            );
+        columnImages_nature[curIdx_nature].classList += " active";
     } else if (array === people) {
         slideShow_img_people.src = people[curIdx_people];
+        for (i = 0; i < columnImages_people.length; i++)
+            columnImages_people[i].className = columnImages_people[i].className.replace(
+                " active",
+                ""
+            );
+        columnImages_people[curIdx_people].classList += " active";
     }
-    slideShow_container.classList.add('fadeIn');
+    slideShow_container.classList.add("fadeIn");
 }
